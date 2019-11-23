c2ee6795082790ed5d566e8e7d6f17d8122c9de8
moebert333 <moebert333@gmail.com>
2019-11-22 23:16:20 -0700

    Update to print results

--- intropyproject-classify-pet-images/print_results.py
+++ intropyproject-classify-pet-images/print_results.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 # */AIPND-revision/intropyproject-classify-pet-images/print_results.py
 #                                                                             
-# PROGRAMMER: 
-# DATE CREATED:
+# PROGRAMMER: Morris Herbert
+# DATE CREATED: Nov 22, 2019
 # REVISED DATE: 
 # PURPOSE: Create a function print_results that prints the results statistics
 #          from the results statistics dictionary (results_stats_dic). It 
